efce0860ab09baf9bc4ca3b9398540e18269a7d6
prasadharbare <prasad1523@proton.me>
2024-12-11 08:15:42 +0000

    ui complete

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,37 +1,67 @@
+import { TbFlareFilled } from "react-icons/tb";
 import Intro from "@/components/Intro";
 
 function Form() {
+  const services = [
+    "Website Design",
+    "Content",
+    "UX Design",
+    "Strategy",
+    "User Research",
+    "Other",
+  ];
+
   return (
     <>
       <Intro />
-      <form className="flex flex-col gap-2">
-        <input type="text"
+      <form className="flex flex-col gap-1">
+        {/* Input */}
+        <input
+          type="text"
           name="fullname"
           id="fullname"
-          placeholder="your name"
-          className="border-stone-700 bg-zinc-50 placeholder:text-slate-700"
+          placeholder="Your name"
+          className="border-b border-stone-700 bg-zinc-50 p-2 placeholder-slate-700 md:bg-lime-400"
           required
         />
-        <input type="email"
+        <input
+          type="email"
           name="email"
           id="email"
-          placeholder="your@gmail.com"
-          className="border-stone-700 md:bg-lime-400 bg-zinc-50 placeholder:text-slate-700"
+          placeholder="your@company.com"
+          className="border-b border-stone-700 bg-zinc-50 p-2 placeholder-slate-700 md:bg-lime-400"
+          required
         />
-        <input type="text"
+        <input
+          type="text"
           name="message"
           id="message"
-          placeholder="Tell us bit about your project"
-          className="h-24 border-stone-700 border-b-2 md:bg-lime-400 bg-zinc-50 placeholder:text-slate-700"
+          placeholder="Tell us a bit about your project..."
+          className="h-24 border-b border-stone-700 bg-zinc-50 p-2 placeholder-slate-700 md:bg-lime-400"
+          required
         />
-        <p className="my-5 text-zinc-800">How can We Help</p>
-        <label >
-          <input type="checkbox"
-            name="website-design"
-            className="size-5"
-          />
-          Website Design
-        </label>
+        <p className="my-5 text-zinc-800">How can we help?</p>
+        {/* Checkbox */}
+        <section className="mb-12 grid grid-cols-2 gap-1 md:max-w-96">
+          {services.map((service, idx) => {
+            return (
+              <label
+                key={service + idx}
+                className="flex cursor-pointer items-center gap-1"
+              >
+                <input type="checkbox" name={service} className="size-6" />
+                {service}
+              </label>
+            );
+          })}
+        </section>
+        <button
+          type="submit"
+          className="flex justify-center gap-2 rounded-lg bg-stone-950 p-2 text-white"
+        >
+          Let's get started{" "}
+          <TbFlareFilled size={20} className="text-lime-500" />
+        </button>
       </form>
     </>
   );
